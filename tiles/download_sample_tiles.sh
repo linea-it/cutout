f5b461251cdfc02254ad4df46457f53c6b7024c5
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2023-03-16 02:15:23 +0000

    Minor fix in download samples script

--- tiles/download_sample_tiles.sh
+++ tiles/download_sample_tiles.sh
@@ -1,7 +1,11 @@
 # curl -sSL "https://scienceserver.linea.org.br/data/cutout_des_sample_tiles.tar.gz" -o cutout_des_sample_tiles.tar.gz \
-wget "https://scienceserver.linea.org.br/data/cutout_des_sample_tiles.tar.gz" -o cutout_des_sample_tiles.tar.gz \
+wget "https://scienceserver.linea.org.br/data/cutout_des_sample_tiles.tar.gz" -O cutout_des_sample_tiles.tar.gz \
+&& echo "Extraindo tar.gz" \
 && tar -xzf cutout_des_sample_tiles.tar.gz \
-&& for z in *.fits.fz; do funpack "$z"; done \
+&& echo "Extraindo fits.fz" \
+&& for z in *.fits.fz; do funpack "$z"; echo "$z"; done \
+&& echo "Removendo arquivos" \
 && rm *.fits.fz \
 && rm cutout_des_sample_tiles.tar.gz \
-&& ls -ltr
+&& ls -lth \
+&& echo "Done!"
